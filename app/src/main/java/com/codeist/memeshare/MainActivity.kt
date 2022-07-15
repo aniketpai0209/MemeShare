8e6c6f677486a203cf44f2c97f04a72f6e232fa4
aniketpai0209 <aniketpai.0209@gmail.com>
2022-07-15 14:31:17 +0530

    User Interface Commit

---
+++ app/src/main/java/com/codeist/memeshare/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.codeist.memeshare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun shareMeme(view: View) {
+
+    }
+
+
+    fun nextMeme(view: View) {
+
+    }
+}
